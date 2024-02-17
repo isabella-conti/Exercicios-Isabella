67ebdef0dfed16cd6cdd7f2101501a895e1a137b
isabella-conti <ircontif@gmail.com>
2024-02-17 15:24:02 -0300

    Exercicio 4

---
+++ Exercicio4.py
@@ -0,0 +1,23 @@
+# 4) Dado o valor de faturamento mensal de uma distribuidora, detalhado por estado:
+
+# SP – R$67.836,43
+# RJ – R$36.678,66
+# MG – R$29.229,88
+# ES – R$27.165,48
+# Outros – R$19.849,53
+
+# Escreva um programa na linguagem que desejar onde calcule o percentual de representação que cada estado teve dentro do valor total mensal da distribuidora.
+faturamento = {
+    "SP": 67836.43,
+    "RJ": 36678.66,
+    "MG": 29229.88,
+    "ES": 27165.48,
+    "Outros": 19849.53,
+}
+
+soma = sum(faturamento.values())
+#print(soma)
+
+for sigla, valor in faturamento.items():
+    porcentagem = (valor/soma)*100
+    print(f'O percentual de representação do estado {sigla} foi de {porcentagem: .2f}% ')
